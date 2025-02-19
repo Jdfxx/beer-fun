b457a254b929d3ae1dd36b34f895d906b9a9713f
Filip <fhagno@yahoo.com>
2025-02-19 08:31:42 +0100

    Add test container mysql test

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -20,7 +20,7 @@
        id varchar(36) not null,
         created_date datetime(6),
         name varchar(255),
-        update_date datetime(6),
+        updated_date datetime(6),
         version integer,
         primary key (id)
     ) engine=InnoDB;
---
+++ src/test/java/pl/filiphagno/spring6restmvc/repositories/MySQLTest.java
@@ -0,0 +1,42 @@
+package pl.filiphagno.spring6restmvc.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.filiphagno.spring6restmvc.entities.Beer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("localmysql")
+public class MySQLTest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:9");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.jpa.hibernate.ddl-auto",()-> "update");
+    }
+
+    @Autowired
+    BeerRepository beerRepository;
+
+
+    @Test
+    void listBeers() {
+        List<Beer> beers = beerRepository.findAll();
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+}
